e19c6487d01a7709d6b42c6c47cb2b69a34d6355
ryokohbato <ryokohbato@gmail.com>
2021-09-27 22:00:56 +0900

    Section 7 演習用

---
+++ normal/webpack.ex3-sample.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const projectDirName = 'ex3-sample';
+
+const baseConfig = require('./webpack.config');
+
+module.exports = {
+  ...baseConfig,
+  
+  entry: `./${projectDirName}/main.js`,
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: `./${projectDirName}/index.html`,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: `${projectDirName}/**/*.(css|png|jpg|jpeg|gif|bmp|tif|tiff)`,
+          to({ context, absoluteFilename }) {
+            return absoluteFilename.replace(`${path.resolve(__dirname)}/${projectDirName}/`, "");
+          },
+        }
+      ]
+    }),
+  ],
+}
